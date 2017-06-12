1770f7c04902a368220aee8046a0329f9b82089e
LowellDev <lbdev@outlook.com>
2017-06-11 19:34:50 -0500

    BugFixes

--- BotLogic.py
+++ BotLogic.py
@@ -9,22 +9,25 @@ def main():
     messages = BotMessages.messages
     bot = GroupMeClasses.generalBot(BotInfo.botID,messages)
     groupme = GroupMeClasses.GroupMe(BotInfo.token,BotInfo.groupID)
-    #groupme.sendMessage('I Started the bot')
+    groupme.sendMessage('I Started the bot')
     if len(sys.argv) >1 :
         previous =groupme.getLastMessageID()
     else:
         previous =''
     count =0
     while True:
-        if count %6==0 : print (count/6)
-        terms =BotMessages.messages
-        hits = bot.searchMessages(terms,groupme.getMessages()['response']['messages'])
-        if hits:
-            print('yes')
-            user=[]
-            for hit in hits:
-                user.append(hit)
-            bot.defaultAction()
+        if count %60==0 : print (count/60)
+        terms =BotMessages.terms
+        #print(terms)
+        newMessages = groupme.getMessages('since_id',previous)
+        if newMessages:
+            hits = bot.searchMessages(terms,newMessages['response']['messages'])
+            if hits:
+                print('yes')
+                user=[]
+                for hit in hits:
+                    user.append(hit)
+                bot.defaultAction()
             groupme.sendMessage(BotMessages.fromCreator,user)
         previous= groupme.getLastMessageID()
         count+=1
